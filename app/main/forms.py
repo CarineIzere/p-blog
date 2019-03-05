e9885a1495573283aa9241913aca7b3135e6c429
CarineIzere <carizeree@gmail.com>
2019-03-05 13:04:34 +0200

    add subscription in main/form.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField,TextAreaField,SubmitField,SelectField
 from ..models import User
 from wtforms import ValidationError
 from wtforms.validators import Required, Email, EqualTo
+from ..models import Subscription
 
 class PostForm(FlaskForm): #create a class that inherits from FlaskForm class
     category = SelectField('Choose Blog Category', choices =[('General','General'),('Life','Life'),('Cars','Cars'),('News','News'),('Music','Music'),('Technology','Technology')],validators=[Required()])
@@ -18,5 +19,14 @@ class CommentsForm(FlaskForm):
 class ContentForm(FlaskForm):
     content = TextAreaField('YOUR POST')
     submit = SubmitField('SUBMIT')
+
+class SubscribeForm(FlaskForm):
+    email = StringField('Email address', validators=[Required(), Email()])
+    submit = SubmitField('Subscribe')
+
+    def validate_email(self, email):
+        email = Subscription.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('That email is already subscribed to our emailing list.')
 
 
