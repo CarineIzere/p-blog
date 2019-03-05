7704271023c98757fba94b97b5b2e7cc66cb7ed4
CarineIzere <carizeree@gmail.com>
2019-03-05 14:26:46 +0200

    add Email configurations

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
-    # Email configurations
+    # 
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
